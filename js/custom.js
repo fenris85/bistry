b83986f3eb4a7a045b47af3f9862bb93e44da0ce
Kuba W <fenris85@gmail.com>
2018-11-27 00:09:57 +0100

    Layout fixes

--- js/custom.js
+++ js/custom.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
 	// Peloader
 	setTimeout(function(){
 		$('body').addClass('loaded');
-	}, 3000);
-	
+	}, 1000);
+
+
 	// Faq Tab
 	$('#faq-area-06  .accordion-single .panel-heading').on('click', function(e){
 		$('#faq-area-06  .accordion-single .panel-heading').removeClass("active");
